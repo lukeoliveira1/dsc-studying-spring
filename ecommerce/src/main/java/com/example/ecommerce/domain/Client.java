72f9ff78c655a2c67966e7d63a45c2cb74566ef1
Lucas <lucasoliveirars22@gmail.com>
2024-12-04 21:16:16 -0300

    client and client repository

--- ecommerce/src/main/java/com/example/ecommerce/domain/Client.java
+++ ecommerce/src/main/java/com/example/ecommerce/domain/Client.java
@@ -21,6 +21,7 @@ public class Client {
 
     private String name;
 
+    @Column(unique = true)
     private String email;
 
     private String cpf;
--- ecommerce/src/main/java/com/example/ecommerce/repository/ClientRepository.java
+++ ecommerce/src/main/java/com/example/ecommerce/repository/ClientRepository.java
@@ -1,9 +1,17 @@
 package com.example.ecommerce.repository;
 
 import com.example.ecommerce.domain.Client;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
+
+    boolean existsByName(String name);
+
+    boolean existsByEmail(String email);
+
+    Page<Client> findAll(Pageable pageable);
 }
